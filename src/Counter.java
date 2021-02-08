8295b16058b3b84e9c0a880fca90ce40a8d04fc7
mplaczek99 <mplaczek99@gmail.com>
2021-02-07 20:09:14 -0600

    Finished Exercise 2

--- src/Counter.java
+++ src/Counter.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Counter {
@@ -10,15 +11,60 @@ public class Counter {
     int n;
 
     ArrayList<Integer> numbers;
+    int[] countingSorted;
+    int[] selectionSorted;
 
-    public Counter(File file) {
+    int fileNum;
+
+    public Counter(File file, int fileNum) {
+        this.fileNum = fileNum;
+
         readFromFile(file);
-        sort();
+        countingSort();
+        selectionSort();
         saveToFile();
     }
 
-    public void sort() {
-        // Sort with O(n) complexity
+    public void countingSort() {
+        int[] countArr = new int[k]; // Defaults to 0
+        countingSorted = new int[n];
+
+        for (Integer num : numbers) {
+            countArr[num] += 1;
+        }
+
+        for (int i = 1; i < k - 1; i ++) {
+            countArr[i + 1] += countArr[i];
+        }
+
+        for (Integer place : numbers) {
+            int num = countArr[place];
+
+            countingSorted[num - 1] = place;
+            countArr[place] --;
+        }
+    }
+
+    public void selectionSort() {
+        selectionSorted = new int[n];
+        for (int i = 0; i < n; i ++) {
+            selectionSorted[i] = numbers.get(i);
+        }
+
+        for (int i = 0; i < n - 1; i ++) {
+            int min = i;
+
+            for (int j = i + 1; j < n; j ++) {
+                if (selectionSorted[j] < selectionSorted[min]) {
+                    min = j;
+                }
+            }
+
+            // Swap the found element with the first element of loop
+            int temp = selectionSorted[min];
+            selectionSorted[min] = selectionSorted[i];
+            selectionSorted[i] = temp;
+        }
     }
 
     public void readFromFile(File file) {
@@ -30,7 +76,6 @@ public class Counter {
             k = sc.nextInt();
 
             while (sc.hasNext()) {
-                // Initialize the array, O(k) complexity
                 numbers.add(sc.nextInt());
             }
 
@@ -40,32 +85,35 @@ public class Counter {
         }
     }
 
-        public void saveToFile() {
-        /*
+    public void saveToFile() {
         try {
-            FileWriter writer = new FileWriter("src/out" + fileNum + ".txt");
+            FileWriter writer = new FileWriter("src/out" + fileNum + "a.txt");
             String isFound;
-            StringBuilder numbersWithSpaces = new StringBuilder();
+            StringBuilder builder = new StringBuilder();
 
-            if (found) {
-                isFound = "Yes";
-            } else {
-                isFound = "No";
+            for (Integer number : countingSorted) {
+                builder.append(number).append(" ");
             }
+            writer.write(builder.toString());
 
-            for (Integer number : numbers) {
-                numbersWithSpaces.append(number).append(" ");
-            }
-
-            writer.write(answer + "\n" + numbersWithSpaces + "\n" + isFound);
-
-            if (found) {
-                writer.write("\n" + num1 + "+" + num2 + "+" + num3 + "=" + answer);
-            }
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-         */
+
+        try {
+            FileWriter writer = new FileWriter("src/out" + fileNum + "b.txt");
+            String isFound;
+            StringBuilder builder = new StringBuilder();
+
+            for (Integer number : selectionSorted) {
+                builder.append(number).append(" ");
+            }
+            writer.write(builder.toString());
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
